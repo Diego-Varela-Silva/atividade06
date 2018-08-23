801b054a7bb4e1a1403d7e51fa029abcde5c566c
Diego Varela <diegovarela.paiva@hotmail.com>
2018-08-23 20:06:14 -0300

    Adiciona teste que falha

--- src/test/java/br/edu/varela/diego/MedianaTest.java
+++ src/test/java/br/edu/varela/diego/MedianaTest.java
@@ -1,6 +1,7 @@
 package br.edu.varela.diego;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class MedianaTest {
 
@@ -8,6 +9,12 @@ public class MedianaTest {
     public void testaEntradaNulaDeveRetornarExcessao() {
         Mediana mediana = new Mediana();
         mediana.calcular(null);
+    }
+
+    public void testaEntradaComIndiceImpar() {
+        Mediana mediana = new Mediana();
+        double [] entrada = new double[]{ 2, 1, 3 };
+        assertEquals(2, mediana.calcular(entrada), 1.0);
     }
 
 }
