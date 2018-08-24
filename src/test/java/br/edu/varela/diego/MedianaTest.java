3ea001133a381372b964e96291f8a6a1fb280cd4
Diego Varela <diegovarela.paiva@hotmail.com>
2018-08-23 21:03:32 -0300

    Adiciona teste menor valor.

--- src/test/java/br/edu/varela/diego/MedianaTest.java
+++ src/test/java/br/edu/varela/diego/MedianaTest.java
@@ -38,4 +38,11 @@ public class MedianaTest {
         assertEquals(2.5, mediana.calcularMedia(entrada), 0.1);
     }
 
+    @Test
+    public void testaEntradaDeveRetornarMenorValor() {
+        Mediana mediana = new Mediana();
+        Double[] entrada = new Double[]{ 2D, 1D, 3D, 4D };
+        assertEquals(1, mediana.calcularMenorValor(entrada), 0.1);
+    }
+
 }
